77b652805a084f978c90da321123071cea4fc34e
Priyanshu501 <76965318+Priyanshu501@users.noreply.github.com>
2024-05-14 21:10:47 +0530

    Completed Logistic Regression

--- Logistic Regression/deployment.py
+++ Logistic Regression/deployment.py
@@ -1,26 +1,28 @@
+''' Deployment of Logistic Regression Model '''
+from pickle import load
 import streamlit as st
 import pandas as pd
-from pickle import load
 
 st.title('Model Deployment: Logistic Regression')
 st.sidebar.header('User Input Parameters')
 
 def user_input_features():
-    PCLASS = st.sidebar.selectbox('Passenger Class',('1','2','3'))
-    CLMSEX = st.sidebar.selectbox('Gender',('Male','Female'))
-    CLMAGE = st.sidebar.number_input("Insert the Age", min_value=0, max_value=100, value=30)
-    SIBSP = st.sidebar.slider('Number of Siblings/Spouses Aboard', min_value=0, max_value=8, value=1)
-    PARCH = st.sidebar.number_input('Number of Parents/Children Aboard', min_value=0, value=0)
-    FARE = st.sidebar.number_input('Fare', min_value=0, value=30)
-    EMBARKED = st.sidebar.selectbox('Embarked', ['C', 'Q', 'S'])
+    ''' Taking User Inputs '''
+    pclass = st.sidebar.selectbox('Passenger Class',('1','2','3'))
+    sex = st.sidebar.selectbox('Gender',('Male','Female'))
+    age = st.sidebar.number_input("Insert the Age", min_value=0, max_value=100, value=30)
+    sibsp = st.sidebar.slider('Number of Siblings/Spouses Aboard', min_value=0, value=1)
+    parch = st.sidebar.number_input('Number of Parents/Children Aboard', min_value=0, value=0)
+    fare = st.sidebar.number_input('Fare', min_value=0, value=30)
+    embarked = st.sidebar.selectbox('Embarked', ['C', 'Q', 'S'])
     data = {
-        'Pclass' : PCLASS,
-        'Sex' : CLMSEX,
-        'Age' : CLMAGE,
-        'SibSp' : SIBSP,
-        'Parch' : PARCH,
-        'Fare' : FARE,
-        'Embarked' : EMBARKED
+        'Pclass' : pclass,
+        'Sex' : sex,
+        'Age' : age,
+        'SibSp' : sibsp,
+        'Parch' : parch,
+        'Fare' : fare,
+        'Embarked' : embarked
     }
     features = pd.DataFrame(data,index = [0])
     features['Sex'] = features['Sex'].map({'Male': 1, 'Female':0})
@@ -28,6 +30,7 @@ def user_input_features():
     return features
 
 def main():
+    ''' Streamlit App '''
     df = user_input_features()
     st.subheader('User Input Parameters')
     st.write(df)
@@ -35,13 +38,17 @@ def main():
     # loading model
     model = load(open('Logistic_Model.sav', 'rb'))
 
+    # Interpreting Prediction
     prediction = model.predict(df)
     if prediction == 1:
         output = "Survived"
     else:
         output = "Did not Survive"
+
+    # Prediction Probability
     prediction_probability = model.predict_proba(df)
 
+    # Displaying Prediction and Prediction Probability
     st.subheader('Predicted Result')
     st.write(output)
 
